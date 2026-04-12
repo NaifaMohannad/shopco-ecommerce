4a1760f143a0df076d5d77458e0a0e3f642773a8
NaifaMohannad <naifamohannad@gmail.com>
2026-04-13 01:24:12 +0300

    Debug Cloudinary connection

--- server/build.sh
+++ server/build.sh
@@ -5,6 +5,17 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+# Test Cloudinary connection
+python manage.py shell -c "
+import cloudinary
+import cloudinary.uploader
+from django.conf import settings
+print('Cloudinary config:')
+print('Cloud name:', settings.CLOUDINARY_STORAGE.get('CLOUD_NAME'))
+print('API Key:', settings.CLOUDINARY_STORAGE.get('API_KEY'))
+print('Storage:', settings.DEFAULT_FILE_STORAGE)
+"
+
 # Create superuser
 python manage.py shell -c "
 from django.contrib.auth.models import User
