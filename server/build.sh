7fbaab7010468782a209ad8bcd5c716201be2426
NaifaMohannad <naifamohannad@gmail.com>
2026-04-12 23:16:55 +0300

    Add products data for Render

--- server/build.sh
+++ server/build.sh
@@ -6,8 +6,8 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py shell -c "
 from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@shopco.com', 'admin123')
+if not User.objects.filter(username='admin1').exists():
+    User.objects.create_superuser('admin', 'naifamohannad@gmail.com', 'admin321')
     print('Superuser created!')
 else:
     print('Superuser already exists!')
