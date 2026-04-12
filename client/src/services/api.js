3482ab826788ae9f01fe820280c7341977c2bf31
NaifaMohannad <naifamohannad@gmail.com>
2026-04-12 23:28:16 +0300

    Update API URL to Render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://shopco-api.onrender.com/api',
 });
 
 // Automatically add JWT token to every request
