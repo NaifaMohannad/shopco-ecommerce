9202c28906c32214c02a643a37b2ce4ee4965840
NaifaMohannad <naifamohannad@gmail.com>
2026-04-12 23:17:58 +0300

    Add loaddata to build.sh

--- server/build.sh
+++ server/build.sh
@@ -4,11 +4,17 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser
 python manage.py shell -c "
 from django.contrib.auth.models import User
 if not User.objects.filter(username='admin1').exists():
-    User.objects.create_superuser('admin', 'naifamohannad@gmail.com', 'admin321')
+    User.objects.create_superuser('admin1', 'naifamohanad@gmail.com', 'admin321')
     print('Superuser created!')
 else:
     print('Superuser already exists!')
 "
+
+# Load products data
+python manage.py loaddata products_data.json
+echo "Products data loaded!"
